da31a0cb88feaf9c1deb562e9a03b82fb18b5cf6
Dan Dart <git@dandart.co.uk>
2021-10-23 14:40:25 +0100

    Better build script

--- build-nix.sh
+++ build-nix.sh
@@ -1,21 +1,67 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+# Show where we were when there was a problem.
+# TODO
+trap pwd ERR
 
 buildDefault() {
     nix-build # | cachix push websites
+    # This gives a warning about --add-root but we already added the root above.
     nix-store -qR --include-outputs $(nix-instantiate) | cachix push websites
 }
 
 buildShell() {
-    nix-build shell.nix #  | cachix push websites
+    nix-build shell.nix -o result-shell #  | cachix push websites
+    # This gives a warning about --add-root but we already added the root above.
     nix-store -qR --include-outputs $(nix-instantiate shell.nix) | cachix push websites
 }
 
-# set -e
-# trap 'exit 1' ERR
+buildReflex() {
+    #if [[ -f shell-ghcjs.nix ]]
+    #then
+    #    #nix-build shell-ghcjs.nix
+    #    #nix-store -qR --include-outputs $(nix-instantiate shell-ghcjs.nix) | cachix push websites
+    #fi
+    #if [[ -f shell-wasm.nix ]]
+    #then
+    #    #nix-build shell-wasm.nix
+    #    #nix-store -qR --include-outputs $(nix-instantiate shell-wasm.nix) | cachix push websites
+    #fi
+    
+    nix-build -A ghc.common -o result/common
+    nix-store -qR --include-outputs $(nix-instantiate -A ghc.common) | cachix push websites
+
+    nix-build -A ghc.backend -o result/backend
+    nix-store -qR --include-outputs $(nix-instantiate -A ghc.backend) | cachix push websites
+
+    nix-build -A ghc.frontend -o result/frontend-ghc
+    nix-store -qR --include-outputs $(nix-instantiate -A ghc.frontend) | cachix push websites
+
+    nix-build -A ghcjs.common -o result/common-ghcjs
+    nix-store -qR --include-outputs $(nix-instantiate -A ghcjs.common) | cachix push websites
+
+    nix-build -A ghcjs.frontend -o result/frontend
+    nix-store -qR --include-outputs $(nix-instantiate -A ghcjs.frontend) | cachix push websites
+
+    # nix-build -A android.frontend -o result/android # Too much memory and OOM crashes
+    # Infinite recursion errors
+    # nix-build -A wasm.common -o result/common-wasm
+    # nix-build -A wasm.frontend -o result/frontend-wasm
+}
+
+buildReflexOrDefault() {
+    if [[ -d external ]]
+    then
+        buildReflex
+    else
+        buildDefault
+    fi
+}
+
 CODEDIR=$PWD/mine
 cd $CODEDIR
 echo Finding Nix projects...
-for FILE in $(find $CODEDIR -name default.nix | grep -v external | grep -v ghcjs | grep -v dist-newstyle)
+for FILE in $(find $CODEDIR -name default.nix | grep -v external | grep -v ghcjs | grep -v dist-newstyle | grep -v wasm-cross | grep -v reflex-platform | grep -v templates)
 do
     DIRLOC=$(dirname $FILE)
     echo Entering $DIRLOC
@@ -31,20 +77,10 @@ do
         echo Building shell.nix...
         buildShell
         echo Building default.nix...
-        if [[ -d external ]]
-        then
-            echo Reflex detected, skipping default build
-        else
-            if [[ -d result ]]
-            then
-                echo result exists, skipping
-            else
-                buildDefault
-            fi
-        fi
+        buildReflexOrDefault
     else
         echo No shell.nix detected, building default.nix
-        buildDefault
+        buildReflexOrDefault
     fi
 done
 cd $CODEDIR
