af03aa205b59609ef8555cd8be416d9979f4b8ea
Dan Dart <git@dandart.co.uk>
2021-10-31 04:16:55 +0000

    add update-reflex

---
+++ update-reflex.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/master.tar.gz -p git -i bash
+set -euxo pipefail
+
+CODEDIR=$PWD/mine
+cd $CODEDIR
+echo Finding Reflex projects...
+for DIR in $(find $CODEDIR -name reflex-platform)
+do
+    echo Entering $DIR
+    cd $DIR
+    git pull
+    cd ..
+    git add reflex-platform
+    git commit -m 'Update reflex' || echo not all
+    git push
+done
+cd $CODEDIR
+echo Done!
