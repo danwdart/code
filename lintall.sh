32a7a12dca377f7bae067b1c6df79b01b3e349a7
Dan Dart <git@dandart.co.uk>
2022-08-21 15:06:40 +0100

    remove a bunch of contrib

--- lintall.sh
+++ lintall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz -p haskell.packages.ghc923.stylish-haskell haskell.packages.ghc923.hlint haskell.packages.ghc902.apply-refact parallel -i bash
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz -p haskell.packages.ghc941.stylish-haskell haskell.packages.ghc941.hlint haskell.packages.ghc902.apply-refact parallel -i bash
 set -e
 mkdir ~/.parallel || echo ".parallel exists"
 touch ~/.parallel/will-cite
--- mine/js/shell.nix
+++ mine/js/shell.nix
@@ -1,10 +1,10 @@
-with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
+with import <nixpkgs> {};
 runCommand "js" {
     buildInputs = [
         nodejs-16_x
         nodePackages.npm # version with node is too old
         nodePackages.npm-check-updates
-        python39 # for npm stuff
+        python310 # for npm stuff
         yarn # haven't decided yet
     ];
 } ""
