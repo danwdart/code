0b6b4a4ba840af897523b0c07a232a5d239b2415
Dan Dart <git@dandart.co.uk>
2023-03-11 22:17:32 +0000

    backup scripts

--- backup-untracked.sh
+++ backup-untracked.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-git submodule foreach --recursive "git ls-files -io --exclude-standard | \
+
+echo "Finding ignored files..."
+
+FILES=$(git submodule foreach --recursive --quiet 'git ls-files -io --exclude-standard | \
     grep -v node_modules | \
     grep -v dist-newstyle | \
     grep -v result | \
@@ -13,6 +16,51 @@ git submodule foreach --recursive "git ls-files -io --exclude-standard | \
     grep -v _stub\\.h$ | \
     grep -v .hie | \
     grep -v log | \
-    grep -v build || echo ''"
+    grep -v vendor | \
+    grep -v gradle | \
+    grep -v .eslintcache | \
+    grep -v .next | \
+    grep -v woocommerce | \
+    grep -v external | \
+    grep -v build | sed -e 's@^@\$displaypath/@'') || exit 1
 
-#  -z | xargs -0 tar rvf
+echo "Finding skipped files..."
+
+FILES_SKIPPED=$(git submodule foreach --recursive --quiet 'git ls-files -v | grep ^S | sed -e "s@^S\s@@g" | \
+    grep -v node_modules | \
+    grep -v dist-newstyle | \
+    grep -v result | \
+    grep -v .direnv | \
+    grep -v hie.yaml | \
+    grep -v \\.o$ | \
+    grep -v \\.a$ | \
+    grep -v \\.hi$ | \
+    grep -v stan.html | \
+    grep -v \\.fd$ | \
+    grep -v _stub\\.h$ | \
+    grep -v .hie | \
+    grep -v log | \
+    grep -v vendor | \
+    grep -v gradle | \
+    grep -v .eslintcache | \
+    grep -v .next | \
+    grep -v woocommerce | \
+    grep -v external | \
+    grep -v build | sed -e 's@^@\$displaypath/@'') || exit 1
+
+IFS=$'\n'
+for FILE in $FILES
+do
+    echo "Adding $FILE..."
+    7z a backup.7z -- "$FILE" >> backup.log 2>backup_error.log
+done
+
+for FILE in $FILES_SKIPPED
+do
+    echo "Adding skipped $FILE..."
+    7z a backup.7z -- "$FILE" >> backup.log 2>backup_error.log
+done
+
+echo Encrypting backup...
+
+gpg -eobackup.7z.gpg -rdan@dandart.co.uk backup.7z
---
+++ decrypt-backup.sh
@@ -0,0 +1,1 @@
+gpg -do backup.7z -u dan@dandart.co.uk backup.7z.gpg
