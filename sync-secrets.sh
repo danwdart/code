1ede5e081dbe7e2c3c9daa16b0b882737919345b
Dan Dart <git@dandart.co.uk>
2023-11-18 04:24:23 +0000

    gh actions secrets and variables syncing script

---
+++ sync-secrets.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# Show where we were when there was a problem.
+# TODO
+trap pwd ERR
+
+help() {
+    echo "Usage: $0 (n)"
+    echo "n: "
+}
+
+ORIG=$(pwd)
+CODEDIRS="$PWD/mine $PWD/contrib"
+NUMCODEDIRS=0
+for CODEDIR in $CODEDIRS
+do
+    ((NUMCODEDIRS+=1))
+done
+CODEDIRNUMBER=0
+for CODEDIR in $CODEDIRS
+do
+    ((CODEDIRNUMBER+=1))
+    # Uncomment to skip
+    # if [ 2 -gt $CODEDIRNUMBER ]; then continue; fi
+
+    cd $CODEDIR
+    echo Finding Nix projects in $CODEDIR...
+    # jobfinder, websites
+    PROJECTS=$(find $CODEDIR -name default.nix | grep -v external | grep -v dist-newstyle)
+    NUMPROJECTS=0
+    for FILE in $PROJECTS
+    do
+        # Double brackets mean as a number, else concat
+        ((NUMPROJECTS+=1))
+    done
+    PROJECTNUMBER=0
+    for FILE in $PROJECTS
+    do
+        # Double brackets mean as a number, else concat
+        ((PROJECTNUMBER+=1))
+
+        # Uncomment to skip
+        if [[ $# > 0 && $1 -gt $PROJECTNUMBER ]]
+        then
+            echo Skipping $FILE
+            continue
+        fi
+
+        DIRLOC=$(dirname $FILE)
+        BASE=$(basename $DIRLOC)
+
+        # if [[ "family" == $BASE ]]; then continue; fi
+
+        # if [[ "chatter" == $BASE || "dubloons" == $BASE || "hs-stdlib" == $BASE || "jobfinder" == $BASE || "9.2.2" == $BASE || "peoplemanager" == $BASE || "tumblr-editor" == $BASE ]]; then continue; fi
+
+        # waiting for https://github.com/NixOS/nixpkgs/issues/197388
+        # if [[ "9.4.2" == $BASE || "peoplemanager" == $BASE ]]; then continue; fi
+
+        PREFIX="$BASE ($PROJECTNUMBER/$NUMPROJECTS) >>> "
+        PREFIX_SED="$BASE ($PROJECTNUMBER\/$NUMPROJECTS) >>> "
+
+        echo "$PREFIX Entering $DIRLOC"
+        cd $DIRLOC
+        if [[ -f .gitmodules ]]
+        then
+            echo "$PREFIX .gitmodules found, updating..."
+            git submodule update --init --recursive
+        fi
+        gh secret set -a actions -f $ORIG/.env.secrets
+        gh variable set -f $ORIG/.env.variables
+    done
+    cd $CODEDIR
+    echo "Finished processing Nix projects in $CODEDIR"
+done
+cd $ORIG
+echo "Finished all"
