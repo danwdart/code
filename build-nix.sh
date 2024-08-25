5e29bce40928c81630f947ab86f4253ef4bf6a82
Dan Dart <d.dart@equalsmoney.com>
2024-08-25 21:17:35 +0100

    cabal-only builds

--- build-nix.sh
+++ build-nix.sh
@@ -4,6 +4,85 @@ set -euo pipefail
 # TODO
 trap pwd ERR
 
+buildCabal_ghc910() {
+    # Not in Nix yet
+    # nix-shell -p "haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])"
+    # nix-shell -p haskell.compilers.ghc910 haskell.packages.ghc910.cabal-install
+    return 0
+}
+
+buildCabal_ghc98() {
+    if [[ "$1" == "compositions" || "$1" == "2021" || "$1" == "onlybase" || "$1" == "onlycore" ]]
+    then
+        echo "$1: ghc too new to do cabal build with ghc98."
+        return 0
+    fi
+
+    if [[ "$1" == "funky-birthdays" || "$1" == "family" ]]
+    then
+        echo "$1: ghc too old to do cabal build with ghc98."
+        return 0
+    fi
+
+    if [[ "$1" == "websites" ]]
+    then
+        echo "$1: compatibility issue for cabal builds - TODO fix."
+        return 0
+    fi
+
+    if [[ "$1" == "monopoly" || "$1" == "projecteuler" || "$1" == "cards" || "$1" == "maths" || "$1" == "games" ]]
+    then
+        echo "$1: hspec issue - TODO fix. Building without tests."
+        nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build --disable-tests"
+        return 0
+    fi
+
+    if [[ "$1" == "coinflicker" ]]
+    then
+        echo "$1: needs to include libGL - however it is that you do that."
+        return 0
+    fi
+
+    if [[ "$1" == "9.8" ]]
+    then
+        echo "$1: needs to include libudev - however it is that you do that."
+        return 0
+    fi
+
+    if [[ "$1" == "reflex-headless" || "$1" == "consolefrp" ]]
+    then
+        echo "$1: TODO needs to add the option in cabal.project to allow newer because of reflex. Building with allow-newer for now."
+        nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build --allow-newer"
+        return 0
+    fi
+
+    if [[ "$1" == "peoplemanager" ]]
+    then
+        echo "$1: TODO update fakedata"
+        return 0
+    fi
+
+    if [[ "$1" == "tumblr-api" ]]
+    then
+        echo "$1: fix humblr"
+        return 0
+    fi
+
+    if [[ "$1" == "js-backend" ]]
+    then
+        echo "$1: requires js backend"
+        return 0
+    fi
+
+    nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build"
+    # nix-shell -p haskell.compilers.ghc98 haskell.packages.ghc98.cabal-install
+}
+
+buildCabal() {
+    buildCabal_ghc910 $1
+    buildCabal_ghc98 $1
+}
+
 buildDefault() {
     nix-shell shell.nix --run "cabal new-build all -j12"
     nix-build -j12  # | cachix push dandart
@@ -82,35 +161,30 @@ do
         DIRLOC=$(dirname $FILE)
         BASE=$(basename $DIRLOC)
 
-        # if [[ "family" == $BASE ]]; then continue; fi
-
-        # if [[ "chatter" == $BASE || "dubloons" == $BASE || "hs-stdlib" == $BASE || "jobfinder" == $BASE || "9.2.2" == $BASE || "peoplemanager" == $BASE || "tumblr-editor" == $BASE ]]; then continue; fi
-
-        # waiting for https://github.com/NixOS/nixpkgs/issues/197388
-        # if [[ "9.4.2" == $BASE || "peoplemanager" == $BASE ]]; then continue; fi
-
         PREFIX="$BASE ($PROJECTNUMBER/$NUMPROJECTS) >>> "
         PREFIX_SED="$BASE ($PROJECTNUMBER\/$NUMPROJECTS) >>> "
 
         echo "$PREFIX Entering $DIRLOC"
-        cd $DIRLOC
+        pushd $DIRLOC
         if [[ -f .gitmodules ]]
         then
             echo "$PREFIX .gitmodules found, updating..."
             git submodule update --init --recursive
         fi
+        echo "$PREFIX Building cabal-only..."
+        buildCabal $BASE 2>&1 | sed "s/^/$PREFIX_SED /g"
         if [[ -f shell.nix ]]
         then
             echo "$PREFIX Building shell.nix..."
-            buildShell $BASE 2>&1 | sed "s/^/$PREFIX_SED /g"
+            buildShell 2>&1 | sed "s/^/$PREFIX_SED /g"
             echo "$PREFIX Building default.nix..."
-            buildDefault $BASE 2>&1 | sed "s/^/$PREFIX_SED /g"
+            buildDefault 2>&1 | sed "s/^/$PREFIX_SED /g"
         else
             echo "$PREFIX No shell.nix detected, building default.nix"
-            buildDefault $BASE 2>&1 | sed "s/^/$PREFIX_SED /g"
+            buildDefault 2>&1 | sed "s/^/$PREFIX_SED /g"
         fi
     done
-    cd $CODEDIR
+    popd
     echo "Finished processing Nix projects in $CODEDIR"
 done
 cd $ORIG
