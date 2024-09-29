9280c9b9fb1b4fa69a46edc36ad19789b74d93c0
Dan Dart <d.dart@equalsmoney.com>
2024-09-29 15:36:49 +0100

    update buildall script

--- build-nix.sh
+++ build-nix.sh
@@ -4,36 +4,15 @@ set -euo pipefail
 # TODO
 trap pwd ERR
 
-buildCabal_ghc910() {
-    # Not in Nix yet
-    # nix-shell -p "haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])"
-    # nix-shell -p haskell.compilers.ghc910 haskell.packages.ghc910.cabal-install
+checkCabal() {
+    # nix-shell -p "haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal check" 2>&1 | sed 's/^/Cabal check: /'
     return 0
 }
 
-buildCabal_ghc98() {
-    if [[ "$1" == "compositions" || "$1" == "2021" || "$1" == "onlybase" || "$1" == "onlycore" ]]
-    then
-        echo "$1: ghc too new to do cabal build with ghc98."
-        return 0
-    fi
-
+buildCabal_ghc910() {
     if [[ "$1" == "funky-birthdays" || "$1" == "family" ]]
     then
-        echo "$1: ghc too old to do cabal build with ghc98."
-        return 0
-    fi
-
-    if [[ "$1" == "websites" ]]
-    then
-        echo "$1: compatibility issue for cabal builds - TODO fix."
-        return 0
-    fi
-
-    if [[ "$1" == "monopoly" || "$1" == "projecteuler" || "$1" == "cards" || "$1" == "maths" || "$1" == "games" ]]
-    then
-        echo "$1: hspec issue - TODO fix. Building without tests."
-        nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build --disable-tests"
+        echo "$1: ghc requirement too old to do cabal build with ghc910."
         return 0
     fi
 
@@ -49,16 +28,44 @@ buildCabal_ghc98() {
         return 0
     fi
 
-    if [[ "$1" == "reflex-headless" || "$1" == "consolefrp" ]]
+    if [[ "$1" == "tumblr-api" ]]
     then
-        echo "$1: TODO needs to add the option in cabal.project to allow newer because of reflex. Building with allow-newer for now."
-        nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build --allow-newer"
+        echo "$1: fix humblr"
         return 0
     fi
 
-    if [[ "$1" == "peoplemanager" ]]
+    if [[ "$1" == "js-backend" ]]
     then
-        echo "$1: TODO update fakedata"
+        echo "$1: requires js backend"
+        return 0
+    fi
+
+    nix-shell -j auto -p "haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build" 2>&1 | sed 's/^/GHC 9.10: /'
+    # nix-shell -p haskell.compilers.ghc910 haskell.packages.ghc910.cabal-install
+}
+
+buildCabal_ghc98() {
+    if [[ "$1" == "compositions" || "$1" == "ffi-quickcheck" || "$1" == "cards" || "$1" == "kasmveh" ||"$1" == "maths" || "$1" == "peoplemanager" || "$1" == "reflex-headless" || "$1" == "projecteuler" || "$1" == "consolefrp" || "$1" == "tumblr-editor" || "$1" == "monopoly" || "$1" == "games" || "$1" == "whatcoffee" || "$1" == "hs-openfaas" || "$1" == "openfaas-examples" || "$1" == "4letters" || "$1" == "bots" || "$1" == "websites" || "$1" == "dubloons" || "$1" == "chatter" || "$1" == "2021" || "$1" == "onlybase" || "$1" == "onlycore" ]]
+    then
+        echo "$1: ghc requirement too new to do cabal build with ghc98."
+        return 0
+    fi
+
+    if [[ "$1" == "funky-birthdays" || "$1" == "family" ]]
+    then
+        echo "$1: ghc requirement too old to do cabal build with ghc98."
+        return 0
+    fi
+
+    if [[ "$1" == "coinflicker" ]]
+    then
+        echo "$1: needs to include libGL - however it is that you do that."
+        return 0
+    fi
+
+    if [[ "$1" == "9.8" ]]
+    then
+        echo "$1: needs to include libudev - however it is that you do that."
         return 0
     fi
 
@@ -74,26 +81,36 @@ buildCabal_ghc98() {
         return 0
     fi
 
-    nix-shell -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build"
+    nix-shell -j auto -p "haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build" 2>&1 | sed 's/^/GHC 9.8: /'
     # nix-shell -p haskell.compilers.ghc98 haskell.packages.ghc98.cabal-install
+}
+
+buildCabal_ghc98_jsbackend() {
+    nix-shell -j auto -p "pkgsCross.pkgsHostBuild.ghcjs.haskell.packages.ghc98.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build" 2>&1 | sed 's/^/GHC 9.8 JS Backend: /'
+}
+
+buildCabal_ghc910_jsbackend() {
+    nix-shell -j auto -p "pkgsCross.pkgsHostBuild.ghcjs.haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-build" 2>&1 | sed 's/^/GHC 9.10 JS Backend: /'
 }
 
 buildCabal() {
     buildCabal_ghc910 $1
     buildCabal_ghc98 $1
+    # buildCabal_ghc910_jsbackend $1
+    # buildCabal_ghc98_jsbackend $1
 }
 
 buildDefault() {
-    nix-shell shell.nix --run "cabal new-build all -j12"
-    nix-build -j12  # | cachix push dandart
+    nix-shell -j auto shell.nix --run "cabal new-build all -j" 2>&1 | sed 's/^/GHC Default: /'
+    nix-build -j auto  # | cachix push dandart
 }
 
 pushShell() {
-    nix-store -qR --include-outputs $(nix-instantiate shell.nix --add-root result-shell --indirect) | cachix push dandart
+    nix-store -qR --include-outputs $(nix-instantiate shell.nix --add-root result-shell --indirect) | cachix push dandart 2>&1 | sed 's/^/pushShell: /'
 }
 
 buildShell() {
-    nix-build shell.nix -o result-shell -j12 #  | cachix push dandart
+    nix-build shell.nix -o result-shell -j auto 2>&1 | sed 's/^/buildShell: /' #  | cachix push dandart
     pushShell &
 }
 
@@ -101,6 +118,13 @@ help() {
     echo "Usage: $0 (n)"
     echo "n: "
 }
+
+nix-channel --update
+# these don't seem to really do anything...
+# nix-store -qR --include-outputs $(nix-instantiate -E "with import <nixpkgs> {}; (haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ]))" --add-root cabalroot --indirect) | cachix push dandart 2>&1 | sed 's/^/pushing cabal: /'
+nix-store -qR --include-outputs $(nix-store -qd $(nix-build -E "with import <nixpkgs> {}; (haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ]))")) | grep -v '\.drv$' | cachix push dandart 2>&1 | sed 's/^/pushing cabal: /'
+# nix-build -E "with import <nixpkgs> {}; (haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ]))" | cachix push dandart 2>&1 | sed 's/^/pushing cabal: /'
+nix-shell -j auto -p "haskell.packages.ghc910.ghcWithPackages (ghc: with ghc; [ cabal-install ])" --run "cabal new-update" 2>&1 | sed 's/^/cabal new-update: /'
 
 ORIG=$(pwd)
 CODEDIRS="$PWD/mine $PWD/contrib"
@@ -127,7 +151,7 @@ do
         # grep -v consolefrp | \
         # grep -v static | \
         grep -v misostuff | \
-        grep -v kasmveh | \
+        # grep -v kasmveh | \
         grep -v ffijs | \
         grep -v haskell-tools | \
         grep -v external | \
@@ -135,7 +159,8 @@ do
         grep -v dist-newstyle | \
         grep -v reflex-platform | \
         grep -v wasm-backend | \
-        grep -v cards-ui
+        grep -v cards-ui | \
+        grep -v yt-sort
         # grep -v tumblr-editor | \
         # grep -v hs-webdriver | \
         )
@@ -172,16 +197,17 @@ do
             git submodule update --init --recursive
         fi
         echo "$PREFIX Building cabal-only..."
-        buildCabal $BASE 2>&1 | sed "s/^/$PREFIX_SED /g"
+        checkCabal $BASE 2>&1 | sed "s/^/$PREFIX_SED: Cabal checking: /g"
+        buildCabal $BASE 2>&1 | sed "s/^/$PREFIX_SED: Cabal: /g"
         if [[ -f shell.nix ]]
         then
-            echo "$PREFIX Building shell.nix..."
-            buildShell 2>&1 | sed "s/^/$PREFIX_SED /g"
-            echo "$PREFIX Building default.nix..."
-            buildDefault 2>&1 | sed "s/^/$PREFIX_SED /g"
+            echo "$PREFIX Nix Shell: Building shell.nix..."
+            buildShell 2>&1 | sed "s/^/$PREFIX_SED: Nix Shell: /g"
+            echo "$PREFIX Nix: Building default.nix..."
+            buildDefault 2>&1 | sed "s/^/$PREFIX_SED: Nix: /g"
         else
             echo "$PREFIX No shell.nix detected, building default.nix"
-            buildDefault 2>&1 | sed "s/^/$PREFIX_SED /g"
+            buildDefault 2>&1 | sed "s/^/$PREFIX_SED: Nix: /g"
         fi
     done
     popd
