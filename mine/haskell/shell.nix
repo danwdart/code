2c70151797f103f64f99f03efa5e8b07c3659494
Dan Dart <git@dandart.co.uk>
2025-08-14 01:16:15 +0100

    lint nix

--- mine/haskell/shell.nix
+++ mine/haskell/shell.nix
@@ -1,6 +1,7 @@
 with import <nixpkgs> {};
 runCommand "haskell" {
     buildInputs = [
-        (haskell.compiler.ghc912 cabal-install)
+        haskell.compiler.ghc912
+        cabal-install
     ];
 } ""
