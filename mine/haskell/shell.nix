b29d5ac067aa8807025ff465b6a4dfbb2c6fd311
Dan Dart <git@dandart.co.uk>
2026-01-07 00:17:40 +0000

    repos

--- mine/haskell/shell.nix
+++ mine/haskell/shell.nix
@@ -1,7 +1,7 @@
-with import <nixpkgs> {};
+with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.zip") {};
 runCommand "haskell" {
     buildInputs = [
-        haskell.compiler.ghc912
+        haskell.compiler.ghc914
         cabal-install
     ];
 } ""
